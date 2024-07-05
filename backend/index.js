04a11bd743f8cc143f0f10d0e56fff0bad6a410e
Amarjeet kaur <amarjeet.kaur111@gmail.com>
2024-07-05 21:59:39 +0400

    Production Configured

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ import connectMongo from  'connect-mongodb-session';
 import { buildContext } from "graphql-passport";
 
 import { configurePassport } from "./passport/passport.config.js";
- 
+import path from "path";
+
+
+const __dirname =  path.resolve(); //means root of the application
 dotenv.config();
 configurePassport();
 
@@ -67,6 +70,16 @@ app.use(
       context: async ({ req , res }) => buildContext({ req, res }),
     }),
   );
+
+  // npm run build will build frontend application and it will be optimized version of the app
+  app.use(express.static(path.join(__dirname,"frontend/dist")));
+
+  app.get('*',(req,res) => {  
+    res.sendFile(path.join(__dirname,"frontend/dist","index.html")); 
+  })
+
+  //In production dont run react application from frontend folder but using npm run build from root package.json
+  //render.com  => backend and frontend under the same domain localhost:4000
 
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
